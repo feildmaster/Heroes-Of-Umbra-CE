722a90389d29542bd3ca04e4186b2b456d33e04f
feildmaster <admin@feildmaster.com>
2015-05-09 04:19:03 -0500

    Add exit message

--- src/com/feildmaster/umbra/Wrapper.java
+++ src/com/feildmaster/umbra/Wrapper.java
@@ -4,6 +4,13 @@ import net.alcuria.online.Main;
 
 public class Wrapper {
     public static void main(String[] args) {
+        try {
+            Class.forName("net.alcuria.online.Main");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Could not find game file, now exiting");
+            System.exit(1);
+        }
+
         // Run the main program
         Main.main(args);
     }
