dfa56e55a61e5ae957100081997de076bd3070d2
feildmaster <admin@feildmaster.com>
2015-05-09 02:55:44 -0500

    Initial commit of CE

---
+++ lib/readme.txt
@@ -0,0 +1,2 @@
+Please download Heroes of Umbra from http://heroesofumbra.com/download and place the jar here.
+Make sure it's named "heroes-of-umbra.jar"
---
+++ src/com/feildmaster/umbra/Wrapper.java
@@ -0,0 +1,11 @@
+package com.feildmaster.umbra;
+
+import net.alcuria.online.Main;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        // Run the main program
+        Main.main(args);
+    }
+
+}
