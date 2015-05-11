f9c3dc7014d34f2d4e1db4a266eaa55654b42811
feildmaster <admin@feildmaster.com>
2015-05-11 04:23:19 -0500

    Exit and print warnings on exceptions.
    Gotta catch them all!

--- src/com/feildmaster/umbra/Wrapper.java
+++ src/com/feildmaster/umbra/Wrapper.java
@@ -6,13 +6,16 @@ public class Wrapper {
     public static void main(String[] args) {
         try {
             Class.forName("net.alcuria.online.Main");
+
+            // Run the main program
+            Main.main(args);
         } catch (ClassNotFoundException ex) {
             System.out.println("Could not find game file, now exiting");
+            System.exit(0);
+        } catch (Exception pokemon) {
+            System.out.println("Exception occured: " + pokemon.getLocalizedMessage());
+            pokemon.printStackTrace();
             System.exit(1);
         }
-
-        // Run the main program
-        Main.main(args);
     }
-
 }
